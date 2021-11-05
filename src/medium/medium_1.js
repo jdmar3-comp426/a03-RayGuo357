280bcc9c1fc2c6d798b109d5f81363c44bdcc654
Raymond Guo <rmguo@live.unc.edu>
2021-11-05 09:39:21 -0400

    Fixed the sort

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,8 +26,8 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    array.sort()
-    if (array.length % 2 == 0) {
+    array.sort((a, b) => a - b)
+    if (array.length % 2 === 0) {
         return (array[array.length/2 - 1] + array[array.length/2]) / 2
     } else {
         return array[Math.floor(array.length/2)]
